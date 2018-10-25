2c84a1a79af16b2f146bcb60c40e7b43465ebd4d
antheboets <44194665+antheboets@users.noreply.github.com>
2018-10-25 12:44:53 +0200

    werkcollege 4

--- website/js/login.js
+++ website/js/login.js
@@ -1,10 +1,14 @@
-var modalList;
-
+var modalList = [];
+modalList.push(document.getElementById("MLogin"));
+modalList.push(document.getElementById("MSignUp"));
+modalList.push(document.getElementById("MForgotPassword"));
 
 //button header/test
+
 document.getElementById("BLogin").addEventListener("click",openMLogin);
 document.getElementById("BSignUp").addEventListener("click",openMSignUp);
 document.getElementById("BForgotPassword").addEventListener("click",openMForgotPassword)
+document.getElementById("BAllowCookies").addEventListener("click",allowCookies);
 //modal input
 document.getElementById("MLoginIUsername").addEventListener("focusout",function(){
 	checkInputIsEmpty(document.getElementById("MLoginIUsername"))
@@ -17,30 +21,40 @@ document.getElementById("MLoginBLogin").addEventListener("click",login);
 document.getElementById("MLoginBClose").addEventListener("click",MLoginClose);
 document.getElementById("MLoginBCrosse").addEventListener("click",MLoginClose);
 document.getElementById("MLoginBForgotPassword").addEventListener("click",openMForgotPassword);
-document.getElementById("MLoginBSingUp").addEventListener("click",);
+document.getElementById("MLoginBSingUp").addEventListener("click",openMSignUp);
 document.getElementById("MSignUpBSingUp").addEventListener("click",singUp);
 document.getElementById("MSignUpBClose").addEventListener("click",MSignUpClose);
 document.getElementById("MSignUpBCrosse").addEventListener("click",MSignUpClose);
-document.getElementById("MSingUpBLogin").addEventListener("click",);
+document.getElementById("MSingUpBLogin").addEventListener("click",openMLogin);
 document.getElementById("MForgotPasswordBForgotPassword").addEventListener("click",forgotPassword);
 document.getElementById("MForgotPasswordBClose").addEventListener("click",MForgotPasswordClose);
 document.getElementById("MForgotPasswordBCrosse").addEventListener("click",MForgotPasswordClose);
 
+function closeAllModals(){
+	modalList.forEach(closeModal);	
+}
+
+function closeModal(item){
+	//https://stackoverflow.com/questions/19506672/how-to-check-if-bootstrap-modal-is-open-so-i-can-use-jquery-validate
+	if ($(item).is(':visible')){
+		$(item).modal('toggle');	
+	}
+}
+
 function openMLogin(){
+	closeAllModals();
 	$('#MLogin').modal({
 		backdrop: 'static'
 	});
 }
 function openMSignUp(){
+	closeAllModals();
 	$('#MSignUp').modal({
 		backdrop: 'static'
 	});
 }
 function openMForgotPassword() {
-	//https://stackoverflow.com/questions/19506672/how-to-check-if-bootstrap-modal-is-open-so-i-can-use-jquery-validate
-	if ($('#MLogin').is(':visible')){
-		MLoginClose();
-	}
+	closeAllModals();
 	$('#MForgotPassword').modal({
 		backdrop: 'static'
 	});
@@ -74,11 +88,10 @@ function checkInputIsEmpty(e){
 		return true;	
 	}
 }
-/*
-function che(){
+function allowCookies(){
+	console.log("test");
+	document.cookie = "cooikesAllowed=true";
+	var x = document.cookie;
 
+	console.log(x);
 }
-function checkLoginPassword(){
-
-}
-*/
