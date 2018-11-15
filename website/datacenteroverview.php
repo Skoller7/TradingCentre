87009199504a79a16470aa25f26d14c96c38a176
arnemergan <mergan.arne@hotmail.com>
2018-11-15 18:16:44 +0100

    update

---
+++ website/datacenteroverview.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="bootstrap-4.1.3/css/bootstrap.min.css">    
+<script src="bootstrap-4.1.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/datacenter.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<?php
+  include_once("header.html");  
+?>
+<div class="containter-fluid">
+<div class="row">
+    <nav class="col-md-3 sidebar">
+        <div class="row">
+                <h3>Overview</h3>
+        <ul>
+            <li><a href="">Best Trades</a></li>
+             <li><a href="">Best Traders</a></li>
+            <li><a href="">Type</a></li>
+            <li><a href="">Most Profit</a></li>
+        </ul>
+        </div>
+    </nav>
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="header-content">
+        <h3>Best Trades</h3>
+        </div>
+        <div class="content-datacenter">
+            <div class="card">
+              <img src="img/tradeimg.png" alt="img of chart" class="img-fluid" height="50%"> 
+              <div class="card-body">
+                <h5 class="card-title">Skoller</h5>
+                <p class="card-text">
+                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. 
+                  </p>
+                <a href="datacenternew.html" class="btn btn-primary">Buy data</a>
+              </div>
+            </div>
+                        <div class="card">
+              <img src="img/tradeimg.png" alt="img of chart" class="img-fluid" height="50%"> 
+              <div class="card-body">
+                <h5 class="card-title">Skoller</h5>
+                <p class="card-text">
+                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. 
+                  </p>
+                <a href="datacenternew.html" class="btn btn-primary">Buy data</a>
+              </div>
+            </div>
+                        <div class="card">
+              <img src="img/tradeimg.png" alt="img of chart" class="img-fluid" height="50%"> 
+              <div class="card-body">
+                <h5 class="card-title">Skoller</h5>
+                <p class="card-text">
+                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. 
+                  </p>
+                <a href="datacenternew.html" class="btn btn-primary">Buy data</a>
+              </div>
+            </div>
+                        <div class="card">
+              <img src="img/tradeimg.png" alt="img of chart" class="img-fluid" height="50%"> 
+              <div class="card-body">
+                <h5 class="card-title">Skoller</h5>
+                <p class="card-text">
+                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. 
+                  </p>
+                <a href="datacenternew.html" class="btn btn-primary">Buy data</a>
+              </div>
+            </div>
+                        <div class="card">
+              <img src="img/tradeimg.png" alt="img of chart" class="img-fluid" height="50%"> 
+              <div class="card-body">
+                <h5 class="card-title">Skoller</h5>
+                <p class="card-text">
+                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. 
+                  </p>
+                <a href="datacenternew.html" class="btn btn-primary">Buy data</a>
+              </div>
+            </div>
+                        <div class="card">
+              <img src="img/tradeimg.png" alt="img of chart" class="img-fluid" height="50%"> 
+              <div class="card-body">
+                <h5 class="card-title">Skoller</h5>
+                <p class="card-text">
+                  Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. 
+                  </p>
+                <a href="datacenternew.html" class="btn btn-primary">Buy data</a>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</body>
+</html>
