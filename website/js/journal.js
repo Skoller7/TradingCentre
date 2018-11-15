f01cd890eaa1aaaf8ab4c3608fb04b3a4fd29e48
arnemergan <mergan.arne@hotmail.com>
2018-11-15 11:02:01 +0100

    update journal

--- website/js/journal.js
+++ website/js/journal.js
@@ -1,4 +1,4 @@
-        function delcard(id){
+/*        function delcard(id){
             document.getElementById(id).style.display = "block";
         }
         function addzero(number){
@@ -13,7 +13,7 @@
                         $(item).modal('toggle');	
                     }
                 }
-        document.getElementById("createnote").addEventListener("click",onclickcreate);
+     document.getElementById("createnote").addEventListener("click",onclickcreate);
         function onclickcreate(){
             var note = document.createElement("div");
             var notebody = document.createElement("div");
@@ -55,11 +55,11 @@
                     $("#createnote").modal('toggle');	
                 });
             }
-        }
+        }*/
 $(function(){
 
 	var dateTo = {
-        portfolioId: "0",
+        portfolioId: "6",
 amount: "200",
 dateFrom: "20/02/2000",
 dateTo: "15/05/2018"
@@ -73,7 +73,7 @@ dateTo: "15/05/2018"
         type: 'GET',
         "headers": {
     		"Content-Type": "application/json",
-            "Authorization": "Bearer " + getCookie("token")
+            "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxNSIsInVuaXF1ZV9uYW1lIjoidGVzdHVzZXIiLCJuYmYiOjE1NDIyNzQyODIsImV4cCI6MTU0MjM2MDY4MiwiaWF0IjoxNTQyMjc0MjgyfQ.n30oCL2z9vkfTEeDKoTOVhgOOxihee3AcDNFe2OH9pMHGGDdl_PN_OXGOflkRP2GeJMW2UZprbNxhNQR-QNKVg"
   		},
         data: JSON.stringify(dateTo),
         dataType: 'json',
@@ -87,8 +87,24 @@ dateTo: "15/05/2018"
                 row.insertCell(2).innerHTML = data[i].currency;
                 row.insertCell(3).innerHTML = data[i].side;
                 row.insertCell(4).innerHTML = data[i].price;
-                row.insertCell(5).innerHTML = data[i].orderqty;
+                row.insertCell(5).innerHTML = data[i].orderQty;
                 row.insertCell(6).innerHTML = data[i].timestamp;
+               var div = document.createElement("div");
+               div.className = "order-type-div";
+                var array = ["Head & Shoulders","Saab","Mercades","Audi"];
+                //Create and append select list
+                var type = document.createElement("select");
+                type.className = "order-type";
+
+                //Create and append the options
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    type.appendChild(option);
+                }
+               div.appendCh
+               row.insertCell(7).innerHTML = div;
             }
         },
         error: function(xhr, ajaxOptions, thrownError){
@@ -98,3 +114,94 @@ dateTo: "15/05/2018"
         }
     });
 });
+function addchart(){ 
+        // based on prepared DOM, initialize echarts instance 
+        var myChart = echarts.init(document.getElementById('main'));
+
+        // specify chart configuration item and data 
+var base = +new Date(1968, 9, 3); 
+var base2 = +new Date(1968,9,4);
+var oneDay = 24 * 3600 * 1000; 
+var date = [];
+var today = +new Date();
+        
+var data = [Math.random() * 300];
+for (var i = base2; i < today; i += oneDay) {
+    var j = 1;
+    var now = new Date(base += oneDay);
+    date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
+    data.push(Math.round((Math.random() - 0.5) * 20 + data[j - 1]));
+    j++;
+}
+option = {
+    tooltip: {
+        trigger: 'axis',
+        position: function (pt) {
+            return [pt[0], '10%'];
+        }
+    },
+    title: {
+        left: 'center',
+        text: 'Binance',
+    },
+    toolbox: {
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: date
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%']
+    },
+    dataZoom: [{
+        type: 'inside',
+        start: 0,
+        end: 10
+    }, {
+        start: 0,
+        end: 10,
+        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+        handleSize: '80%',
+        handleStyle: {
+            color: '#fff',
+            shadowBlur: 3,
+            shadowColor: 'rgba(0, 0, 0, 0.6)',
+            shadowOffsetX: 2,
+            shadowOffsetY: 2
+        }
+    }],
+    series: [
+        {
+            name:'Won/Lost',
+            type:'line',
+            smooth:true,
+            symbol: 'none',
+            sampling: 'average',
+            itemStyle: {
+                color: 'rgb(255, 70, 131)'
+            },
+            areaStyle: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgb(255, 158, 68)'
+                }, {
+                    offset: 1,
+                    color: 'rgb(255, 70, 131)'
+                }])
+            },
+            data: data
+        }
+    ]
+};
+        // use configuration item and data specified to show chart
+        myChart.setOption(option);
+}
