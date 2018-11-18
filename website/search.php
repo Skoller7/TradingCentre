032713ef91b2d26dabd67a13602ce9baaa8064be
antheboets <44194665+antheboets@users.noreply.github.com>
2018-11-18 18:25:17 +0100

    replacing elements with external files

---
+++ website/search.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Tradingcentre</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/search.css">
+		<link rel="stylesheet" type="text/css" href="css/header.css">
+		<script src="js/lib.js" type="text/javascript"></script>
+		<script src="js/jquery.js" type="text/javascript"></script>
+	    <script src="bootstrap-4.1.3/js/bootstrap.min.js" type="text/javascript"></script>
+	    <?php 
+	    	include("favicon.html");
+	    ?>
+	</head>
+	<body>
+		<?php 
+			include("header.html");
+		?>
+		<script src="js/header.js" type="text/javascript"></script>
+		<div id="user">
+			
+		</div>
+		<script src="js/search.js" type="text/javascript"></script>
+		<?php 
+			include("footer.html");
+		?>
+	</body>
+</html>
