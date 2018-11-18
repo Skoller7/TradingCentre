6481edf3c973030b090ddc0e87139905e7f8a67f
Ilja <ilja.derycke@gmail.com>
2018-11-18 22:59:38 +0100

    Uploading (non-working) progress on github to make sure I don't lose anything

--- website/js/journal.js
+++ website/js/journal.js
@@ -1,73 +1,76 @@
-        function delcard(id){
-            document.getElementById(id).style.display = "block";
-        }
-        function addzero(number){
-            if(number < 10){
-                number = "0" + number;
-            }
-            return number;
-        }
-                function closeModal(item){
-                    //https://stackoverflow.com/questions/19506672/how-to-check-if-bootstrap-modal-is-open-so-i-can-use-jquery-validate
-                    if ($(item).is(':visible')){
-                        $(item).modal('toggle');	
-                    }
-                }
-        document.getElementById("createnote").addEventListener("click",onclickcreate);
-        function onclickcreate(){
-            var note = document.createElement("div");
-            var notebody = document.createElement("div");
-            var notetitle = document.createElement("h5");
-            var notetext = document.createElement("p");
-            var notedel = document.createElement("button");
-            var notecontent = document.getElementById("notecontent").value;
-            if(notecontent == ""){
-                document.getElementById('falsenote').style.color = "red";
-                document.getElementById('falsenote').innerHTML = '* This field cannot be empty!';
-            
-            }else{
-                var ID = Math.random();
-                document.getElementById('falsenote').innerHTML = '';
-                note.className = "card";
-                note.setAttribute("id",ID);
-                notebody.className = "card-body";
-                notetitle.className = "card-title";
-                notetext.className = "card-text";
-                notedel.className = "btn btn-primary";
-                 //notedel.setAttribute("onclick",delcard(ID););
-                var today = new Date();
-                var dd = today.getDate();
-                var mm = today.getMonth() +1;
-                var yyyy = today.getFullYear();
-                var hh = addzero(today.getHours());
-                var MM = addzero(today.getMinutes());
-                today = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + MM;
-                notetitle.innerHTML = today + '<br>';
-                notetext.innerHTML =  notecontent;
-                notedel.innerHTML = "X";
-                document.getElementById("notes").appendChild(note);
-                note.appendChild(notebody);
-                notebody.appendChild(notetitle);
-                notebody.appendChild(notetext);
-                notebody.appendChild(notedel);
-                document.getElementById("notecontent").value = "";
-                $(function(){
-                    $("#createnote").modal('toggle');	
-                });
-            }
-        }
+function delcard(id){
+    document.getElementById(id).style.display = "block";
+}
+function addzero(number){
+    if(number < 10){
+        number = "0" + number;
+    }
+    return number;
+}
+function closeModal(item){
+    //https://stackoverflow.com/questions/19506672/how-to-check-if-bootstrap-modal-is-open-so-i-can-use-jquery-validate
+    if ($(item).is(':visible')){
+        $(item).modal('toggle');	
+    }
+}
+document.getElementById("createnote").addEventListener("click",onclickcreate);
+function onclickcreate(){
+    var note = document.createElement("div");
+    var notebody = document.createElement("div");
+    var notetitle = document.createElement("h5");
+    var notetext = document.createElement("p");
+    var notedel = document.createElement("button");
+    var notecontent = document.getElementById("notecontent").value;
+    if(notecontent == ""){
+        document.getElementById('falsenote').style.color = "red";
+        document.getElementById('falsenote').innerHTML = '* This field cannot be empty!';
+    }
+    else{
+        var ID = Math.random();
+        document.getElementById('falsenote').innerHTML = '';
+        note.className = "card";
+        note.setAttribute("id",ID);
+        notebody.className = "card-body";
+        notetitle.className = "card-title";
+        notetext.className = "card-text";
+        notedel.className = "btn btn-primary";
+         //notedel.setAttribute("onclick",delcard(ID););
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() +1;
+        var yyyy = today.getFullYear();
+        var hh = addzero(today.getHours());
+        var MM = addzero(today.getMinutes());
+        today = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + MM;
+        notetitle.innerHTML = today + '<br>';
+        notetext.innerHTML =  notecontent;
+        notedel.innerHTML = "X";
+        document.getElementById("notes").appendChild(note);
+        note.appendChild(notebody);
+        notebody.appendChild(notetitle);
+        notebody.appendChild(notetext);
+        notebody.appendChild(notedel);
+        document.getElementById("notecontent").value = "";
+        $(function(){
+            $("#createnote").modal('toggle');	
+        });
+    }
+}
 $(function(){
 
 	var dateTo = {
-        dateTo:"01/01/0002"
+        portfolioId: "0",
+amount: "200",
+dateFrom: "20/02/2000",
+dateTo: "15/05/2018"
 	}
 	//http://localhost:5000/api/auth/login
 	console.log(dateTo);
     $.ajax({
     	"async": true,
   		"crossDomain": true,
-  		url: 'http://localhost:5000/api/orders/get',
-        type: 'POST',
+  		url: 'http://10.3.50.6/api/order/get',
+        type: 'GET',
         "headers": {
     		"Content-Type": "application/json",
             "Authorization": "Bearer " + getCookie("token")
@@ -82,9 +85,10 @@ $(function(){
                 row.insertCell(0).innerHTML = data[i].exchange;
                 row.insertCell(1).innerHTML = data[i].symbol;
                 row.insertCell(2).innerHTML = data[i].currency;
-                row.insertCell(3).innerHTML = data[i].price;
-                row.insertCell(4).innerHTML = data[i].qty;
-                row.insertCell(5).innerHTML = data[i].timePlaced;
+                row.insertCell(3).innerHTML = data[i].side;
+                row.insertCell(4).innerHTML = data[i].price;
+                row.insertCell(5).innerHTML = data[i].orderqty;
+                row.insertCell(6).innerHTML = data[i].timestamp;
             }
         },
         error: function(xhr, ajaxOptions, thrownError){
