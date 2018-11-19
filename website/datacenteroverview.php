dc338c482ec99e14d6cfb216e19512bdf167a82c
arnemergan <mergan.arne@hotmail.com>
2018-11-19 15:15:01 +0100

    update journal & datacenter & header

--- website/datacenteroverview.php
+++ website/datacenteroverview.php
@@ -3,6 +3,9 @@
 <head>
 <link rel="stylesheet" href="bootstrap-4.1.3/css/bootstrap.min.css">    
 <script src="bootstrap-4.1.3/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="js/lib.js" type="text/javascript"></script>
 <link rel="stylesheet" href="css/datacenter.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
