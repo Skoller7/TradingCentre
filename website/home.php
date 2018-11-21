aa5cee12cfe89719d7218dd443614069823e132d
antheboets <anthe.boets@gmail.com>
2018-11-21 22:05:56 +0100

    updating css links

--- website/home.php
+++ website/home.php
@@ -10,11 +10,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap-4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
     <link rel="stylesheet" type="text/css" href="css/datacenter.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="bootstrap-4.1.3/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/header.css">
+ 
     <script src="js/lib.js" type="text/javascript"></script>
     <?php 
       include('favicon.html');
