48cbd48a191fb345dc86cccf96b3baecf7642ee0
IljaDR <38908439+IljaDR@users.noreply.github.com>
2018-12-05 22:51:56 +0100

    Finished the index page
    Added promotional text to the index page and finished the layout and code

--- website/Index.php
+++ website/Index.php
@@ -25,7 +25,7 @@
 		<link rel="stylesheet" type="text/css" href="css/datacenter.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script src="js/jquery.js" type="text/javascript"></script>
-		<!--<script src="js/index.js" type="text/javascript"></script>-->
+		<script src="js/index.js" type="text/javascript"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	    <script src="bootstrap-4.1.3/js/bootstrap.min.js" type="text/javascript"></script>
 	    <script src="js/lib.js" type="text/javascript"></script>
@@ -38,7 +38,7 @@
 				include('header.html');
 			?>
 			
-		<div class="parallax">
+		<div class="parallax" style="transform-style: preserve-3d;">
 			<div id="background" class="parallax__layer parallax__layer--back">
 				<div id="leftPanel" class="showcasePanel">
 				</div>
@@ -49,21 +49,23 @@
 			</div>
 			
 			
-			<div class="parallax__layer parallax__layer--base">
+			<div class="parallax__layer">
 				<div id="promoLeft" class="promo">
 					<img src="img/journal.png" class="promoLogo">
-					<p class="promoText">Dit is tekst dat van zo'n hoog emotioneel kwaliteit is, dat de gebruiker niet anders kan dan op de knop hieronder te drukken</p>
-					<button class="promoButton" onclick="scrollToJournal()">Journal</button>
+					<p class="promoText">Log your trades on your exchanges to find trading patterns or just to keep track of your buying and selling history</p>
+					<input type="button" class="promoButton" id="promoJournalButton" value="Journal"></input>
 				</div>
 				<div id="promoRight" class="promo">
 					<img src="img/datacentre.png" class="promoLogo">
-					<p class="promoText">De kwaliteit van deze tekst is nog een stuk hoger; het zou me niet verbazen als het in de geschiedenisboeken bij MacBeth en Lord of the Rings gezet wordt</p>
-					<button class="promoButton" onclick="scrollToDatacenter()">Datacenter</button>
+					<p class="promoText">Learn new trading techniques from experienced traders or get paid to teach others about your favorite strategies</p>
+					<input type="button" class="promoButton" id="promoDatacenterButton" value="Datacenter"></input>
 				</div>
 				<div id="demoJournal" class="demo">
 					<div id="journalLeft" class="demoHalves">
 						<div class="demoText">
-							<p>Hier staat een uitzonderlijk boeiende tekst dat duidelijk uitlegt hoe de journal werkt.</p>
+							<p>Your journal will automatically keep track of your trades on BitMEX and generate handy charts so you can see your progress over time. 
+							Additionally, you can log your trades in custom portfolios based on goals or techniques.</p>
+							<a href="" style="text-decoration: underline;">Get started</a>
 						</div>
 					</div>
 					<div id="journalRight" class="demoHalves">
@@ -75,7 +77,8 @@
 					</div>
 					<div id="datacenterRight" class="demoHalves">
 						<div class="demoText">
-							<p>Hier staat een uitzonderlijk boeiende tekst dat duidelijk uitlegt hoe de journal werkt.</p>
+							<p>Buy expert trading data via a smartcontract on the NEO blockchain or share your own expertise by creating curated portfolios with custom notes and images paired to each trade. </p>
+							<a href="" style="text-decoration: underline;">Get started</a>
 						</div>
 					</div>
 				</div>
@@ -93,6 +96,6 @@
 			GDPR info
 		</div>
 		-->
-		<script src="js/index.js" type="text/javascript"></script>
+		
 	</body>
 </html>
