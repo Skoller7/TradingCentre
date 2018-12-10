3da6f4b4d83826494cd3a03fca9e0cb9d4b460e8
IljaDR <38908439+IljaDR@users.noreply.github.com>
2018-12-10 01:21:52 +0100

    Added Profile, committing the index files again

--- website/js/index.js
+++ website/js/index.js
@@ -33,8 +33,12 @@ function autoLogin(username, password){
 }
 */
 
-var scroll_to_demodatacenter = document.getElementById('demoDatacenter');
+$(document).ready(function(){
 
+
+document.getElementById('promoJournalButton').addEventListener("click", scrollToJournal);
+document.getElementById('promoDatacenterButton').addEventListener("click", scrollToDatacenter);
+setSize();
 function scrollToJournal(){
 	var scroll_to_demojournal = document.getElementById('demoJournal');
 	scroll_to_demojournal.scrollIntoView({behavior: "smooth"});
@@ -49,3 +53,28 @@ function allowCookies(){
 	document.cookie = "cooikesAllowed=true;  expires=Thu, 01 Jan 3000 00:00:00 UTC";
 	document.getElementById("cookieBanner").style.display = "none";
 }
+
+$(window).resize(function() {
+	setSize();
+});
+
+
+var journalImg = document.getElementById("journalRight");
+
+function setSize(){
+
+var height = $(window).height()
+if(height < 640)
+	height = 640;
+$('.promo').css('height',height);
+
+$('#demoJournal').css('margin-top',height);
+$('#demoJournal').css('height',height);
+$('#demoDatacenter').css('height',height);
+
+var topPosition = parseFloat($('#journalRight').position().top) * 1.54;
+$('#journalImg').css('top',topPosition + 'px');
+topPosition = parseFloat($('#datacenterLeft').position().top) * 1.485;
+$('#datacenterImg').css('top',topPosition + 'px');
+}
+}); 
