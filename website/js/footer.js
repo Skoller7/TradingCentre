9da3c0c9ed95ff4ba7aa2bacbebc4262fb566b1f
Ilja <ilja.derycke@gmail.com>
2018-12-17 12:20:12 +0100

    Updated footer.js so it is actually responsive to the content on the page

--- website/js/footer.js
+++ website/js/footer.js
@@ -1,11 +1,9 @@
-//Checks if the footer will appear on screen; if so, the footer gets moved to below the screen4 
+$(document).ready(function(){
+	//Checks if the footer will appear on screen; if so, the footer gets moved to below the screen4 
 if(parseInt($('#footer').css('top'), 10) < $(window).height()){
 	$('#footer').css('top',$(window).height() + 10);
 }
-
-
-
-$(document).ready(function(){
+	console.log($('#footer').offset().top);
 	window.scrollTo(0,0);
 	for(var i = 0; i < document.getElementsByClassName('footerSignup').length; i++){
 		document.getElementsByClassName('footerSignup')[i].addEventListener("click", openMSignUp);
