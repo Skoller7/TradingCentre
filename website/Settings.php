fd87adef384af2ae1de06fdf27f0446513a7f4e1
antheboets <anthe.boets@gmail.com>
2018-12-18 18:07:48 +0100

    Commiting Settings from Fernando since he had problems with merging
    Fernando work in an older version of his branch and would cause merging confits if he would commit his changes.
    
    Co-Authored-By: feerns <feerns@users.noreply.github.com>

--- website/Settings.php
+++ website/Settings.php
@@ -19,12 +19,12 @@
 ?>
    
             <div class="vertical-settings">
-            <a href="" id="general" class="active">General</a>
-            <a href="" id="security">Security</a>
-            <a href="" id="privacy">Privacy</a>
-            <a href="" id="account">Account</a>
+                <button class="tablink" onclick="openOption(event, 'general')" id="default">General</button>
+                <button class="tablink" onclick="openOption(event, 'security')">Security</button>
+                <button class="tablink" onclick="openOption(event, 'privacy')">Privacy</button>
+                <button class="tablink" onclick="openOption(event, 'account')">Account</button>
             </div>
-            <div id="general-option" class="option-settings">
+            <div id="general" class="content">
                 <form>
                     <a><label for="language">Preferred language:</label>
                     <select id="language">
@@ -43,34 +43,44 @@
                         <option value="btc">BTC</option>
                         <option value="eth">ETH</option>
                     </select></a><br>
-                    <input type="submit">
+                    <a><input type="submit"></a>
                 </form>
             </div>
-            <div id="security-option" class="option" hidden>
+            <div id="security" class="content">
                 <form>
                     <a><label for="skey">Change secret key: </label>
                     <input type="text" id="skey"></a><br>
-                    <input type="submit">
+                    <a><input type="submit"></a>
                 </form>
             </div>
-            <div id="privacy-option" class="option" hidden>
+            <div id="privacy" class="content">
                 <form>
-                    <a><label for="public">Do you want your profile to be public? If it is not public, your data will not be able to be sold!</label>
+                    <a><label for="public">Do you want your profile to be public?
+                        If it is not public, your data will not be able to be sold!</label><br>
                     <input type="text" id="public"></a><br>
-                    <input type="submit">
+                    <a><input type="submit"></a>
                 </form>
             </div>
-            <div id="account-option" class="option" hidden>
-                <form>
-                    <a><label for="name">Change your profile's name: </label> 
-                    <input type="text" id="name"></a>
-                    <a><label for="img">Change your profile's name: </label>
-                    <input type="image" id="img"></a>
+            <div id="account" class="content">
+                <form id="accountForm" onsubmit="return validateForm()">
+                    <a><label for="name">Change your first name: </label> 
+                    <input type="text" id="fname" name="firstName"></a>
+                    <a><label for="name">Change your last name: </label> 
+                    <input type="text" id="lname" name="lastName"></a>
+                    <a><label for="name">Change your e-mail: </label> 
+                    <input type="email" id="email" name="email"></a>
+                    <a><label for="name">Change your user name: </label> 
+                    <input type="text" id="uname" name="username"></a>
+                    <a><label for="pw">Change your phone number: </label>
+                    <input type="number" id="pn" name="phone"></a>
                     <a><label for="pw">Change your password: </label>
-                    <input type="password" id="pw"></a>
-                    <a><label for="description">Change your profile's description</label>
-                    <textarea id="description"></textarea></a>
-                    <input type="submit">
+                    <input type="password" id="pw" name="password"></a>
+                    <a><label for="img">Give your new profile foto URL: </label>
+                    <input type="url" id="img" name="image"></a>
+                    <a><label for="description">Change your profile's description</label><br>
+                    <textarea id="description" rows="4" cols="50" name="description"></textarea></a>
+                    <a><input type="submit"></a>
                 </form>
             </div>
+    <script src="js/Settings.js" type="text/javascript"></script>
 </body>
