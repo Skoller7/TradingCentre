096592e5acd9ce3c3349e8817f05b9e9cac3f57f
IljaDR <38908439+IljaDR@users.noreply.github.com>
2018-12-18 01:39:10 +0100

    Fixed footer on index.php, started working on faq.php

--- website/Index.php
+++ website/Index.php
@@ -32,82 +32,79 @@
 	    	include('favicon.html');
 	     ?>
 	</head>
-		<body>
-			<?php 
-				include('header.html');
-			?>
-			
-		<div class="parallax" style="transform-style: preserve-3d;">
-			<div id="background" class="parallax__layer parallax__layer--back">
-				<div id="leftPanel" class="showcasePanel">
-				</div>
-				<div id="rightPanel" class="showcasePanel">
-				</div>
-			</div>
-			<div id="divider" class="parallax__layer parallax__layer--back">
-			</div>
-			
-			
-			<div class="parallax__layer">
-				<div id="promoLeft" class="promo">
-					<img src="img/journal.png" class="promoLogo">
-					<p class="promoText">Log your trades on your exchanges to find trading patterns or just to keep track of your buying and selling history</p>
-					<input type="button" class="promoButton" id="promoJournalButton" value="Journal"></input>
-				</div>
-				<div id="promoRight" class="promo">
-					<img src="img/datacentre.png" class="promoLogo">
-					<p class="promoText">Learn new trading techniques from experienced traders or get paid to teach others about your favorite strategies</p>
-					<input type="button" class="promoButton" id="promoDatacenterButton" value="Datacenter"></input>
-				</div>
-				<div id="demoJournal" class="demo">
-					<div id="journalLeft" class="demoHalves">
-						<div class="demoText">
-							<p>Your journal will automatically keep track of your trades on BitMEX and generate handy charts so you can see your progress over time. 
-							Additionally, you can log your trades in custom portfolios based on goals or techniques.</p>
-							<a class="getStarted" style="text-decoration: underline;">Get started</a>
-						</div>
-					</div>
-					<div id="journalRight" class="demoHalves">
-					</div>
-				</div>
-				<div id="horizontalDivider"><div></div></div>
-				<div id="demoDatacenter" class="demo">
-					<div id="datacenterLeft" class="demoHalves">
-					</div>
-					<div id="datacenterRight" class="demoHalves">
-						<div class="demoText">
-							<p>Buy expert trading data via a smartcontract on the NEO blockchain or share your own expertise by creating curated portfolios with custom notes and images paired to each trade. </p>
-							<a class="getStarted" style="text-decoration: underline;">Get started</a>
-						</div>
-					</div>
-				</div>
-				<?php 
-            		include('footer.html');
-        		?>
-			</div>
-			<div class="parallax__layer parallax__layer--front demo">
-				<div id="journalImg">
-				</div>
-				<div id="datacenterImg">
-				</div>
-			</div>
-			
-		</div>
-
-		<!--
-		<div id="cookieBanner">
-			<div class="button" id="BAllowCookies">Allow cookies</div>
-			GDPR info
-		</div>
-		-->
-		<script>
-			
-			var verificationKey = "";
-		</script>
+	<body>
 		<?php 
-			if(isset($_GET['verificationKey'])){
-				echo '<script>verificationKey = "'. $_GET['verificationKey'] .'";verifieEmail();</script>';
-			}
-		?>		
+			include('header.html');
+		?>
+		
+	<div class="parallax" style="transform-style: preserve-3d;">
+		<div id="background" class="parallax__layer parallax__layer--back">
+			<div id="leftPanel" class="showcasePanel">
+			</div>
+			<div id="rightPanel" class="showcasePanel">
+			</div>
+		</div>
+		<div id="divider" class="parallax__layer parallax__layer--back">
+		</div>
+		
+		
+		<div class="parallax__layer">
+			<div id="promoLeft" class="promo">
+				<img src="img/journal.png" class="promoLogo">
+				<p class="promoText">Log your trades on your exchanges to find trading patterns or just to keep track of your buying and selling history</p>
+				<input type="button" class="promoButton" id="promoJournalButton" value="Journal"></input>
+			</div>
+			<div id="promoRight" class="promo">
+				<img src="img/datacentre.png" class="promoLogo">
+				<p class="promoText">Learn new trading techniques from experienced traders or get paid to teach others about your favorite strategies</p>
+				<input type="button" class="promoButton" id="promoDatacenterButton" value="Datacenter"></input>
+			</div>
+			<div id="demoJournal" class="demo">
+				<div id="journalLeft" class="demoHalves">
+					<div class="demoText">
+						<p>Your journal will automatically keep track of your trades on BitMEX and generate handy charts so you can see your progress over time. 
+						Additionally, you can log your trades in custom portfolios based on goals or techniques.</p>
+						<a class="getStarted" style="text-decoration: underline;">Get started</a>
+					</div>
+				</div>
+				<div id="journalRight" class="demoHalves">
+				</div>
+			</div>
+			<div id="horizontalDivider"><div></div></div>
+			<div id="demoDatacenter" class="demo">
+				<div id="datacenterLeft" class="demoHalves">
+				</div>
+				<div id="datacenterRight" class="demoHalves">
+					<div class="demoText">
+						<p>Buy expert trading data via a smartcontract on the NEO blockchain or share your own expertise by creating curated portfolios with custom notesand images paired to each trade. </p>
+						<a class="getStarted" style="text-decoration: underline;">Get started</a>
+					</div>
+				</div>
+			</div>
+			<?php 
+            include('footer.html');
+        ?>
+		<div class="parallax__layer parallax__layer--front demo">
+			<div id="journalImg">
+			</div>
+			<div id="datacenterImg">
+			</div>
+		</div>
+		
+	</div>
+	<!--
+	<div id="cookieBanner">
+		<div class="button" id="BAllowCookies">Allow cookies</div>
+		GDPR info
+	</div>
+	-->
+	<script>
+		var verificationKey = "";
+	</script>
+	<?php 
+		if(isset($_GET['verificationKey'])){
+			echo '<script>verificationKey = "'. $_GET['verificationKey'] .'";verifieEmail();</script>';
+		}
+	?>		
 	</body>
 </html>
---
+++ website/faq.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/datacenter.css">
+    <link rel="stylesheet" href="bootstrap-4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="bootstrap-4.1.3/js/bootstrap.min.js"></script>
+    <script src="js/lib.js" type="text/javascript"></script>
+    <?php
+		include_once('favicon.html');
+	?>
+</head>
+<body>
+<?php
+  include_once("header.html");
+?>
+	<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="col-md-4"></div>
+		<div class="col-md-4 col-sm-12 col-xs-12">
+			test
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+<?php
+  include_once("footer.html");
+?>
+</body>
+<!-- <script src="js/web3.min.js"></script>
+<script src="js/truffle-contract.js"></script>
+<script src="js/datacenterbuy.js"></script>
+<script src="js/datacenter.js"></script> -->
+</html>
