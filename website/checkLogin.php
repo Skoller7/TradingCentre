5c812c4443ac7f8d24806397a2b43c5931c6cac8
antheboets <44194665+antheboets@users.noreply.github.com>
2018-12-19 14:44:36 +0100

    small bugfix

--- website/checkLogin.php
+++ website/checkLogin.php
@@ -7,7 +7,7 @@
 				$_SESSION['jwtToken'] = $_POST['jwtToken'];
 				$expireTime = time() + 60*60*24;
 				$_SESSION['jwtTokenExpireTime'] = $expireTime;
-				setcookie('jwtToken',$_POST['jwtToken'], $expireTime;;
+				setcookie('jwtToken',$_POST['jwtToken'], $expireTime);
 				
 				if(isset($_POST['verificationKey']) && $_POST['email']){
 					$_SESSION['verificationKey'] = $_POST['verificationKey'];
