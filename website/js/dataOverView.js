f296d5e9cd720e3920e2bcfcd8e026a456a154fd
Robin <Robin.b97@outlook.com>
2018-12-20 01:41:52 +0100

    Update dataOverView.js
    works completely

--- website/js/dataOverView.js
+++ website/js/dataOverView.js
@@ -85,7 +85,7 @@ requestData: function(){
 
     $('#exampleModalCenter').show();
     $('.makemodal').click();
-
+    portfolioId = [1, 2 ,3];
     //default values.
     dataDatum = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     dataInput = [1, 5, 8, 9, 2, 8, 12, 25, 9, 18, 28, 15];
@@ -146,19 +146,20 @@ calcProfit: function(){
   })
 },
 
-requestPrice: function(){
-
-
-  //retrieve the data from the database (which porfolio's he owns).
-
-  App.contracts.DataContract.at('0x15781269bc3516278309224ad450a88e1de01fad').then(function(instance){
-    return instance.getPrice.call()
-  }).then(priceOfData => {
-    console.log("Succesfully retrieved price of data :", priceOfData);
-    $('#contractPrice').text(priceOfData);
-    $('#contractPrice').val("Buy for " + priceOfData + " WEI");
-  })
-},
+//commented no really needed anymore.
+// requestPrice: function(){
+//
+//
+//   //retrieve the data from the database (which porfolio's he owns).
+//
+//   App.contracts.DataContract.at('0x15781269bc3516278309224ad450a88e1de01fad').then(function(instance){
+//     return instance.getPrice.call()
+//   }).then(priceOfData => {
+//     console.log("Succesfully retrieved price of data :", priceOfData);
+//     $('#contractPrice').text(priceOfData);
+//     $('#contractPrice').val("Buy for " + priceOfData + " WEI");
+//   })
+// },
 
 getcard: function(){console.warn();
     var data = makerequestnopar("http://10.3.50.6/api/portfolio?soldOnly=true","GET",token);
@@ -266,41 +267,126 @@ $(function() {
 
 
 /*
-display card on screen
+dummy portfolio
 */
 
+// function getcard(){console.warn();
+//     var data = makerequestnopar("http://10.3.50.6/api/portfolio?soldOnly=true","GET",token);
+//     if(getstatus() == 401){
+//         openMLogin();
+//     }else{
+//         if(getstatus() == 400 || getstatus() == 500 || getstatus() == 501){
+//             alert("Something went wrong, please try again later");
+//         }else{
+//             arrayforsaleown = data;
+//             App.setcontentcards(arrayforsaleown,"see-more-own",true,max_own,n_own);
+//         }
+//     }
+// }
+//
+//
+//
+// setcontentcards: function(arrayport,seemoreid,boolown,max,n){
+//     var seemore = document.getElementById(seemoreid);
+//     var lengtharray = arrayport.length / 6;
+//     lengtharray = lengtharray.toFixed(0);
+//     if(lengtharray == 1 || lengtharray == 0){
+//         max = arrayport.length;
+//     }
+//     for(var i = n; i < max;i++){
+//             if(arrayport[i].address != null && arrayport[i].address != ""){
+//                     App.setcard(arrayport,i,boolown);
+//             }
+//     }
+//     lengtharray--;
+//     seemore.style.display = "none";
+//     seemore.innerHTML = "";
+//     if(lengtharray > 0){
+//             var a = document.createElement("a");
+//             a.setAttribute("href","#");
+//             a.setAttribute("style","margin:0 auto;");
+//             a.innerHTML = "See more";
+//             a.addEventListener("click",function(){
+//                 n = max;
+//                 if(lengtharray > 1){
+//                     max = max + 6;
+//                 }else{
+//                     max = arrayport.length;
+//                 }
+//                 App.setcontentcards(arrayport,seemoreid,boolown,max,n);
+//             });
+//             seemore.style.display = "block";
+//             seemore.appendChild(a);
+//     }
+// },
+//
+//
+// setcard: function(data,i,own){
+//                var card = document.createElement("div");
+//                 card.setAttribute("class","col-md-3 col-sm-12 card");
+//                 var img = document.createElement("img");
+//                 img.setAttribute("alt","image of trade");
+//                 img.setAttribute("class","img-fluid");
+//                 img.setAttribute("height","50%");
+//                 img.setAttribute("src",data[i].imgURL);
+//                 card.appendChild(img);
+//                 var cardbody = document.createElement("div");
+//                 cardbody.setAttribute("class","card-body");
+//                 if(own){
+//                     cardbody.innerHTML +=  "<h5 class='card-title'>"+data[i].name+"</h5>";
+//                 }else{
+//                     cardbody.innerHTML +=  "<h5 class='card-title'>skoller</h5>";
+//                 }
+//
+//                 App.contracts.DataContract.at(portfolios[i].address).then(function(instance){
+//                   DataContractInstance = instance;
+//                   console.log("inside instance");
+//                   DataContractInstance.getBuyersCount.call().then(function(r){
+//                     buyersCountf = r;
+//                     DataContractInstance.getPrice.call().then(function(r2){
+//                     profitf = buyersCountf * r2;
+//                     pricef = r2;
+//                     console.log(buyersCountf, pricef, profitf);
+//
+//                   cardbody.innerHTML +=  "<p class='card-text' id='portfoliodata'>Buyers count: "+ buyersCountf +"<br />Profit made: " + profitf +"</p>";
+//                   cardbody.innerHTML += "<a href='datacenternew.php?portfolioId="+data[i].portfolioId+"' class='btn btn-primary'>Show data</a>";
+//                   card.appendChild(cardbody);
+//
+//                   })
+//                 })
+//               })
+//                 if(own){
+//                     cards.appendChild(card);
+//                 }
+// }
+//
 
 
 
 function addbarchart(){
-    var myChart = echarts.init(document.getElementById('main'), 'light');
+  setTimeout(function() {
+  option = {
+      title: {
+          text: 'Data sold per month'
+      },
+      tooltip: {},
+      legend: {
+          data:['Data sold']
+      },
+      xAxis: {
+          data: dataDatum
+      },
+      yAxis: {},
+      series: [{
+          name: 'Data sold',
+          type: 'bar',
+          data: dataInput
+      }]
+  },
 
-    var option = {
-        title:{
-            text: 'Data sold per month',
-            left: 'left'
-        },
-        toolbox: {
-            feature:{
-                show: true,
-                saveAsImage: {
-                    title: 'Save As Image'
-                }
-            }
-        },
-        legend: {
-            data:['Data sold']
-        },
-        xAxis: {
-            data: dataDatum
-        },
-        yAxis:{},
-        series: [{
-            name:'Data sold',
-            type: 'bar',
-            data: dataInput
-        }]
-    };
+      myChart.setOption(option);
+      }, 500);
 
-        myChart.setOption(option);
-}
+      var myChart = echarts.init(document.getElementById('main'), 'light');
+
+  }
