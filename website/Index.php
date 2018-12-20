a347ccd3848c5d65fc84242632857f8944297f42
antheboets <44194665+antheboets@users.noreply.github.com>
2018-12-20 20:02:07 +0100

    Update Index.php

--- website/Index.php
+++ website/Index.php
@@ -1,14 +1,24 @@
-<?php 
+<?php
 	session_start();
 	if(isset($_POST['jwtToken'])){
-		$_SESSION['jwtToken'] = $_POST['jwtToken'];
-		$_COOKIE['jwtToken'] = $_POST['jwtToken'];
-		header('location: journal.php');
+		$_SESSION['jwtToken'] = $_COOKIE['jwtToken'];
+		$_SESSION['jwtTokenExpireTime'] = $_COOKIE['jwtTokenExpireTime'];
 	}
-
-	if (session_status() == PHP_SESSION_NONE) {
-		if(isset($_SESSION['jwtToken'])){
-			if(!empty($_SESSION['jwtToken'])){
+	//checks that the session jwtToken is set and if the cookie jwtToken is not not the same it wil put the session token in the cookie
+	//if the session token is expired it wil redirect te user to the index page
+	if(isset($_SESSION['jwtToken']) && isset($_SESSION['jwtTokenExpireTime'])){
+		if(!empty($_SESSION['jwtToken']) && !empty($_SESSION['jwtTokenExpireTime'])){
+			if(time() < $_SESSION['jwtTokenExpireTime']){
+				if($_SESSION['jwtToken'] != $_COOKIE['jwtToken']){
+					setcookie('jwtToken',$_SESSION['jwtToken'], $_SESSION['jwtTokenExpireTime']);
+					setcookie('jwtTokenExpireTime',$_SESSION['jwtTokenExpireTime'],$_SESSION['jwtTokenExpireTime']);
+				}
+			}
+		}
+	}
+	else{
+		if(isset($_COOKIE['jwtToken']) && isset($_COOKIE['jwtTokenExpireTime'])){
+			if(!empty($_COOKIE['jwtToken']) && !empty($_COOKIE['jwtTokenExpireTime'])){
 				echo "<head>";
 				echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
 				echo "</head>";
@@ -22,7 +32,7 @@
 					type: 'GET',
 					'headers': {
 					    'Content-Type': 'application/json',
-					    'Authorization': 'Bearer ".$_SESSION['jwtToken']."',
+					    'Authorization': 'Bearer ".$_COOKIE['jwtToken']."',
 
 				  },
 				    success: function(){
@@ -30,57 +40,19 @@
 				        form.setAttribute('method','POST');
 				        form.setAttribute('action','".(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"."');
 				        var inputJwtToken = document.createElement('INPUT');
-		    			inputJwtToken.setAttribute('type','hidden');
-		    			inputJwtToken.setAttribute('name','jwtToken');
-		    			inputJwtToken.setAttribute('value','true');
-		    			form.appendChild(inputJwtToken);
-		    			document.body.appendChild(form);
-		    			form.submit();	
+	        			inputJwtToken.setAttribute('type','hidden');
+	        			inputJwtToken.setAttribute('name','jwtToken');
+	        			inputJwtToken.setAttribute('value','true');
+	        			form.appendChild(inputJwtToken);
+	        			document.body.appendChild(form);
+	        			form.submit();	
 				    }
 				});
 				</script>";
 				echo "</body>";
 			}
 		}
-	}
-	else{
-		if(isset($_COOKIE['jwtToken'])){
-			if(!empty($_COOKIE['jwtToken'])){
-				echo "<head>";
-				echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
-				echo "</head>";
-				echo "<body>";
-				echo "<script>
-				$.ajax({
-					'async': true,
-					'crossDomain': true,
-					url: 'http://10.3.50.6/api/user/search?username=test',
-					timeout : 0,
-					type: 'GET',
-					'headers': {
-					    'Content-Type': 'application/json',
-					    'Authorization': 'Bearer ".$_SESSION['jwtToken']."',
-
-				  },
-				    success: function(){
-			  			var form = document.createElement('FORM');
-				        form.setAttribute('method','POST');
-				        form.setAttribute('action','".(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"."');
-				        var inputJwtToken = document.createElement('INPUT');
-		    			inputJwtToken.setAttribute('type','hidden');
-		    			inputJwtToken.setAttribute('name','jwtToken');
-		    			inputJwtToken.setAttribute('value','true');
-		    			form.appendChild(inputJwtToken);
-		    			document.body.appendChild(form);
-		    			form.submit();	
-				    }
-				});
-				</script>";
-				echo "</body>";	
-			}
-			//check if still valid
-		}
-	}
+	} 
 ?>
 <html>
 	<head>
