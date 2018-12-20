1f5f7294c302a134bfd252af6374e33a5ed97a64
Robin <Robin.b97@outlook.com>
2018-12-20 14:23:09 +0100

    oi
    working on the datalibrary but still errors sometimes

--- website/js/datalibrary.js
+++ website/js/datalibrary.js
@@ -22,6 +22,7 @@
    var max_own = 6;
    var n_other = 0;
    var max_other = 6;
+   var adressen = [];
 
 
 App = {
@@ -62,33 +63,33 @@ initContract1: function(){
     App.contracts.DataContract = TruffleContract(DataContractArtifact);
     App.contracts.DataContract.setProvider(App.web3Provider);
     console.log(App.contracts);
-    App.isUserBacker();
+  //  App.isUserBacker();
     App.getcard();
   })
 },
-
-isUserBacker: function(){
-
-  App.contracts.DataContract.at('0x15781269bc3516278309224ad450a88e1de01fad').then(function(instance){
-    DataContractInstance = instance;
-
-      web3.eth.getAccounts(function(error, accounts) {
-        var account = accounts[0];
-    DataContractInstance.backers.call(account).then(function(r){
-      if(r == false){
-          $('.btn-buycheck').show();
-          $('#buyCheck').show();
-        console.log('Person is a backer');
-      }
-      else {
-          $('.btn-fault').show();
-      }
-    }).catch(function(err){
-      console.log(err);
-    });
-  });
-})
-},
+//
+// isUserBacker: function(){
+//
+//   App.contracts.DataContract.at('0x15781269bc3516278309224ad450a88e1de01fad').then(function(instance){
+//     DataContractInstance = instance;
+//
+//       web3.eth.getAccounts(function(error, accounts) {
+//         var account = accounts[0];
+//     DataContractInstance.backers.call(account).then(function(r){
+//       if(r == false){
+//           $('.btn-buycheck').show();
+//           $('#buyCheck').show();
+//         console.log('Person is a backer');
+//       }
+//       else {
+//           $('.btn-fault').show();
+//       }
+//     }).catch(function(err){
+//       console.log(err);
+//     });
+//   });
+// })
+// },
 
 getcard: function(){console.warn();
     makerequestnopar("http://10.3.50.6/api/purchase", "GET", token, function(data2){
@@ -99,11 +100,13 @@ getcard: function(){console.warn();
     makerequestnopar("http://10.3.50.6/api/portfolio?portfolioId=" + data2[j].portfolioId ,"GET",token, function(dummyvar){
 
     console.log(dummyvar);
+    adressen.push(dummyvar.address);
     data.push(dummyvar);
 
       }, false);
     }
   } , false);
+  console.log(adressen);
 
     console.log(data);
     if(getstatus() == 401){
@@ -168,20 +171,71 @@ setcard: function(data,i){
                 var cardbody = document.createElement("div");
                 cardbody.setAttribute("class","card-body");
 
-
-
                   cardbody.innerHTML +=  "<h5 class='card-title'>"+data[i].name+"</h5>";
                   cardbody.innerHTML +=  "<p class='card-text' id='portfoliodata'>Buyers count: bla"+  +"<br />Profit made: blabla" +  +"</p>";
-                  cardbody.innerHTML +=  "<a href='datacenternew.php?portfolioId="+data[i].portfolioId+"' class='btn btn-primary'>Show data</a>";
-                  cardbody.innerHTML +=  "<div class='btn-check' id='buyCheck'>  <a class='btn btn-buycheck btn-primary' id='buyCheck' href='boughtdata.php'>View data</a></div> <div class='btn-fault' id='faultcheck'> <p> Either you are not logged in in metamask,    Or you are not an actual buyer of this portfolio!<span><a href=''> Click here for help </a> </span></p>  </div>"
+                  // cardbody.innerHTML +=  "<a href='datacenternew.php?portfolioId="+data[i].portfolioId+"' class='btn btn-primary'>Show data</a>";
 
+
+                  var btncheck = document.createElement("div");
+                  btncheck.setAttribute("class", "btn-check");
+                  btncheck.setAttribute("id", "buyCheck");
+                  btncheck.style.display="none";
+
+                  cardbody.appendChild(btncheck);
+
+                  var acheck = document.createElement("a");
+                  acheck.setAttribute("class", "btn btn-buycheck btn-primary");
+                  acheck.setAttribute("id", "buyCheck");
+                  acheck.setAttribute("href", "boughtdata.php");
+                  acheck.innerHTML = "View Data";
+
+                  btncheck.appendChild(acheck);
+
+                  var btnfault = document.createElement("div");
+                  btnfault.setAttribute("id", "faultcheck");
+                  btnfault.style.display="none";
+
+                  cardbody.appendChild(btnfault);
+
+                  var pcheck = document.createElement("p");
+                  pcheck.innerHTML = "Either you are not logged in in metamask, Or you are not an actual buyer of this portfolio!<span><a href=''> Click here for help </a> </span></p>";
+
+                  btnfault.appendChild(pcheck);
 
 
                   card.appendChild(cardbody);
                   cards.appendChild(card);
 
-                  $('.btn-buycheck').hide();
-                  $('.btn-fault').hide();
+                  console.log(adressen.length);
+                  for(var h = 0; h < adressen.length; h++){
+
+                  App.contracts.DataContract.at(adressen[h]).then(function(instance){
+                    DataContractInstance = instance;
+
+                      web3.eth.getAccounts(function(error, accounts) {
+                        var account = accounts[0];
+                        console.log(account);
+                    DataContractInstance.backers.call(account).then(function(r){
+                      console.log(r);
+                      setTimeout(function(){
+                      if(r){
+                          btncheck.style.display="block";
+                          // $('.btn-buycheck').show();
+                          // $('#buyCheck').show();
+                        console.log('Person is a backer');
+                      }
+                      else if(r == false){
+                          btnfault.style.display="block";
+                          // $('.btn-fault').show();
+                      }
+                    }, 50)
+                  }).catch(function(err){
+                      console.log(err);
+                    });
+                  });
+                })
+              }
+
 }
 
 
