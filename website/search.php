ea0e36067397fecc5c93a5268a0aa49b519f0c87
antheboets <anthe.boets@gmail.com>
2018-12-20 02:11:15 +0100

    added extra styling and fixed footer position

--- website/search.php
+++ website/search.php
@@ -7,8 +7,8 @@
 		<link rel="stylesheet" type="text/css" href="css/datacenter.css">
 		<link rel="stylesheet" type="text/css" href="css/footer.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="js/lib.js" type="text/javascript"></script>
-		<script src="js/jquery.js" type="text/javascript"></script>
 	    <script src="bootstrap-4.1.3/js/bootstrap.min.js" type="text/javascript"></script>
 	    <script src="js/search.js" type="text/javascript"></script>
 	    <?php 
@@ -16,10 +16,10 @@
 	    ?>
 	</head>
 	<body>
+		
 		<?php 
 			include("header.html");
 		?>
-		<script src="js/header.js" type="text/javascript"></script>
 		<script>
 			<?php
 
@@ -35,20 +35,17 @@
 				
 			?>
 		</script>
-
-		<div class="container">
-
-			<div class="row">
-				<h1 class="col-md-10" id="search">Searched username: </h1>
-			</div>
-			<div class="row">
-			
-			
-				<div class="col-md-1"></div>
-				<div class="col-md-10" id="user"></div>
+		<div style="min-height: 100%">
+			<div class="container" id="searchContent">
+				<div class="row">
+					<h1 class="col-md-10" id="searchedUsername">Searched username: </h1>
+				</div>
+				<div class="row">
+					<div class="col-md-1"></div>
+					<div class="col-md-10" id="user"></div>
+				</div>
 			</div>
 		</div>
-		
 		<?php
 			include ('footer.html');
 		?>
